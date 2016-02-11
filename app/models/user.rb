b635f88665efe1a4a579a502ffa57d64d08434b2
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-11 16:02:55 +0100

    Show all practice data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ActiveRecord::Base
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
     self.role ||= :user
   end
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, #:confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
