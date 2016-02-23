2e8a0ead73c01d7bb2d6e7c739a1efbcff4f3c0c
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-23 14:16:56 +0100

    Add validation for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  validates :name, presence: true
+
   def set_default_role
     self.role ||= :user
   end
