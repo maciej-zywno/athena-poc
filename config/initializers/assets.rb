f0ba6321c210e07ac9745fe29b391e3e3bc4d982
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-23 09:16:59 +0100

    Move images to vendor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf|jpg)\z/
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf|jpg|png)\z/
