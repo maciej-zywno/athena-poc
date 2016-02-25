73051452d034050af34701e9cf57550e587c4820
Maciej Zywno <m.zywno@gmail.com>
2016-02-25 16:36:01 +0100

    fill with data fetched from Alchemy

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,12 @@
 class Answer < ActiveRecord::Base
-  has_one :alchemy
+  after_create :alchemy
+
+  has_one :fill_alchemy_keywords_and_sentiment
+
+  private
+
+    def fill_alchemy_keywords_and_sentiment
+      self.keywords = AlchemyAPI.search(:keyword_extraction, text: self.answer)
+      self.sentiment = AlchemyAPI.search(:sentiment_analysis, text: self.answer)
+    end
 end
