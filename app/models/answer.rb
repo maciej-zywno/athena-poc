e7959d52cc790f319bf14adbd9303adc3fd35371
Maciej Zywno <m.zywno@gmail.com>
2016-02-25 17:21:47 +0100

    fill answers with alchemy data

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,13 @@
 class Answer < ActiveRecord::Base
+  after_commit :fill_alchemy_keywords_and_sentiment
+
   has_one :alchemy
+
+  private
+
+    def fill_alchemy_keywords_and_sentiment
+      keywords = AlchemyAPI.search(:keyword_extraction, text: self.answer)
+      sentiment = AlchemyAPI.search(:sentiment_analysis, text: self.answer)
+      Alchemy.create!(answer_id: self.id, keywords: keywords, sentiment: sentiment)
+    end
 end
