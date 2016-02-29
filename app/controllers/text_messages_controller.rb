36eed0228143a7f0d513cdf4145d2bdfd16c26cc
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-29 16:59:33 +0100

    Remove warnings

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -4,7 +4,7 @@ class TextMessagesController < ApplicationController
 
     twilio_client = Twilio::REST::Client.new ENV['TWILLO_SID'], ENV['TWILLO_AUTH']
 
-    twilio_client.account.sms.messages.create(
+    twilio_client.account.messages.create(
       from: ENV['TWILLO_FROM_PHONE'],
       to: number_to_send_to,
       body: "This is an message. It gets sent to #{number_to_send_to}"
