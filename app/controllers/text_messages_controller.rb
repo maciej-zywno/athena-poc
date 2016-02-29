96f31a70cd88cf092c55bbb5b58df7943bee0e6c
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-29 18:22:14 +0100

    Change TWILLO to TWILIO

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -2,10 +2,10 @@ class TextMessagesController < ApplicationController
   def create
     number_to_send_to = params[:text_message][:phone_number]
 
-    twilio_client = Twilio::REST::Client.new ENV['TWILLO_SID'], ENV['TWILLO_AUTH']
+    twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH']
 
     twilio_client.account.messages.create(
-      from: ENV['TWILLO_FROM_PHONE'],
+      from: ENV['TWILIO_FROM_PHONE'],
       to: number_to_send_to,
       body: "This is an message. It gets sent to #{number_to_send_to}"
     )
