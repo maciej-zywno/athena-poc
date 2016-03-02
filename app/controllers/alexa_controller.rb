19b954382c9ac910f5400cc176fbeda50e885e02
Maciej Zywno <m.zywno@gmail.com>
2016-03-02 10:53:07 +0100

    refactor

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -2,15 +2,14 @@ class AlexaController < ApplicationController
   skip_before_action :verify_authenticity_token, :authenticate_user!
 
   def handle
-    request_body = request.body.read.to_s
-
-    logger.info 'FIND USER BY AMAZONE USER ID'
-    user = User.find_by_amazon_user_id(request.body['user']['userId'])
-    logger.info user.inspect
-
     verify_correct_alexa_request!(request_body)
     request_body = JSON.parse(request.body.read.to_s)
     log_request(request_body)
+
+    logger.info 'FIND USER BY AMAZON USER ID'
+    user = User.find_by_amazon_user_id(request.body['user']['userId'])
+    logger.info user.inspect
+
     alexa_request = AlexaRubykit.build_request(request_body)
     render text: handle_alexa_request(alexa_request)
   end
