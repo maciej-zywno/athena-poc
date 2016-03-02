522c3be5dbe5c7b12b6b9b22ef97aef1531a88de
Maciej Zywno <m.zywno@gmail.com>
2016-03-03 00:23:38 +0100

    use avsmetrics

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -3,6 +3,8 @@ class AlexaController < ApplicationController
 
   def handle
     Alexa::Verifier.verify(headers: request.headers, body: request.body.read)
+    avsmetrics(request.body.read)
+
     request_body = request.body.read.to_s
     request_body_parsed = JSON.parse(request_body)
     log_request(request_body_parsed)
@@ -12,42 +14,42 @@ class AlexaController < ApplicationController
 
   def handle_alexa_request(request_body, request)
     case request.type
-      when 'LAUNCH_REQUEST' then process_alexa_request(*Alexa::LaunchRequestWrapper.new(request_body, request).process)
-      when 'INTENT_REQUEST' then process_alexa_request(*Alexa::IntentRequestWrapper.new(request_body, request).process)
-      when 'SESSION_ENDED_REQUEST' then process_alexa_request(nil, {}, true)
+      when 'LAUNCH_REQUEST' then alexa_response(*Alexa::LaunchRequestWrapper.new(request_body, request).process)
+      when 'INTENT_REQUEST' then alexa_response(*Alexa::IntentRequestWrapper.new(request_body, request).process)
+      when 'SESSION_ENDED_REQUEST' then alexa_response(nil, {}, true)
       else raise "unsupported request type #{request.type}"
     end
   end
 
   private
 
-    def process_alexa_request(speech, session_attributes, session_end)
+    def alexa_response(speech, session_attributes, session_end)
       response = AlexaRubykit::Response.new
       response.add_speech(speech) if speech
       session_attributes.each { |k, v|
         response.add_session_attribute(k, v)
       }
-
-      response = response.build_response(session_end)
-
-      logger.info "---------------------------------------------------"
-      logger.info "---------------------------------------------------"
-      logger.info "---------------------------------------------------"
-      logger.info "speech=#{speech}"
-      logger.info "session_attributes=#{session_attributes}"
-      logger.info response.inspect
-      response
+      response.build_response(session_end)
     end
 
     def log_request(request_body)
       logger.info '!!! ALEXA REQUEST BODY !!!'
-
       logger.info request_body.to_json
-      logger.info "request.headers['SignatureCertChainUrl']=#{request.headers['SignatureCertChainUrl']}"
-      logger.info "request.headers['Signature']=#{request.headers['Signature']}"
-
       logger.info '!!! ALEXA END !!!'
     end
 
+    def avsmetrics(json)
+      require 'net/http'
+      require 'uri'
+
+      token = 'monarch-health-token-1'
+      url = "http://avsmetrics.herokuapp.com/alexa?token=#{token}"
+      uri = URI.parse(url)
+
+      request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' =>'application/json')
+      request.body = json
+      http = Net::HTTP.new(uri.host, uri.port)
+      resp = http.request(request)
+    end
 end
 
