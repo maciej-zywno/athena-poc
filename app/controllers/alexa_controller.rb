6ab181dd33dd30b050083c7e1e364d6fa98e580a
Maciej Zywno <m.zywno@gmail.com>
2016-03-02 12:53:25 +0100

    temp

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -32,11 +32,23 @@ class AlexaController < ApplicationController
       logger.info "request.slots=#{request.slots}"
       logger.info "request.name=#{request.name}"
 
+      # response.add_session_attribute('questions', question_id_and_questions_pairs)
+      # response.add_session_attribute('answered_questions', [])
+      # response.add_session_attribute('current_question', first_question[0])
+
+      # add current answer to answered_questions
+      current_answered_questions = request.session.attributes['answered_questions']
+      current_question_id, current_question_text = request.session.attributes['current_question']
+      current_question_answer = request.slots['Answer']['value']
+      new_answered_questions = current_answered_questions << [current_question_id, current_question_answer]
+      request.session.attributes['answered_questions'] = new_answered_questions
+
       response = AlexaRubykit::Response.new
-      response.add_speech(@questions.first)
-      response.add_session_attribute('current_question', @questions.first)
-      response.add_session_attribute(@questions.first, request.slots['Answer']['value'])
-      response.build_response(@questions.first.nil?)
+      next_question = any_not_answered_question(request.session.attributes['questions'], request.session.attributes['answered_questions'])
+      response.add_speech(next_question)
+      response.add_session_attribute('current_question', next_question)
+
+      response.build_response(session_end = all_questions_answered?(request.session.attributes['questions'], request.session.attributes['answered_questions']))
     end
 
     def handle_launch_request(request_body_parsed, request)
@@ -49,12 +61,17 @@ class AlexaController < ApplicationController
       user = User.find_by_amazon_user_id(request_body_parsed['session']['user']['userId'])
 
       logger.info 'FIND TREATMENT QUESTIONS'
-      question_id_and_questions_pairs = user.treatments.last.questions.pluck(:id, :question)
+      # [[4, "On a scale 1 to 10 how bad is your back today?"], [1, "How do you feel today?"]]
+      question_id_and_questions_pairs = Treatment.last.questions.pluck(:id, :question)
+      next_question = question_id_and_questions_pairs[0]
 
       response = AlexaRubykit::Response.new
-      response.add_speech(@questions.first)
-      response.add_session_attribute('current_question', @questions.first)
-      @questions.shift
+      response.add_speech(next_question[1])
+
+      response.add_session_attribute('questions', question_id_and_questions_pairs)
+      response.add_session_attribute('answered_questions', [])
+      response.add_session_attribute('current_question', [next_question[0], next_question[1]])
+
       response.build_response(session_end = false)
     end
 
@@ -77,5 +94,18 @@ class AlexaController < ApplicationController
 
       logger.info '!!! ALEXA END !!!'
     end
+
+    # all_questions: [[4, "On a scale 1 to 10 how bad is your back today?"], [1, "How do you feel today?"]]
+    # answered_questions: [[1, "foo"], [4, "bar"]]
+    def all_questions_answered?(all_questions, answered_questions)
+      logger.info "!!! ALL_QUESTIONS_ANSWERED? !!!"
+      logger.info "all_questions=#{all_questions.inspect}"
+      logger.info "answered_questions=#{answered_questions.inspect}"
+    end
+
+    def any_not_answered_question(all_questions, answered_questions)
+      return false
+    end
+
 end
 
