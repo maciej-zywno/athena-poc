0e8d250f9c25a87ca191c3d6fe25f5415406a048
Maciej Zywno <m.zywno@gmail.com>
2016-03-04 14:58:35 +0100

    uber

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,16 +1,10 @@
-class UberController < ApplicationController
-  skip_before_action :verify_authenticity_token, :authenticate_user!
-
-  def handle
+class Client
+  def instance
     client = Uber::Client.new do |config|
       config.server_token  = 'wk8nwLSCOQUMAKUbtSQYfISSk8yosMzDeB8RnHhN'
       config.client_id     = 'pDgNR8fvL2s3qarXh23IamF7zWTrtRCd'
       config.client_secret = 'wAC-f6nfNOrqoz-APF84lNL37R7eFCEM8tC6ORT4'
     end
-
-
-    # render text: handle_alexa_request(request_body_parsed, AlexaRubykit.build_request(request_body_parsed))
   end
 
 end
-
