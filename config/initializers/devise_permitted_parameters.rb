66b1ec5297efe1a6b70a5931fcee1fe123a2f73f
Maciej Zywno <m.zywno@gmail.com>
2016-03-04 18:47:19 +0100

    kids

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,7 +8,7 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :athena_id, :athena_secret])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :athena_id, :athena_secret, :phone_number])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 
