14497b0a8713c2e441a021546571bab5ea9e126d
Maciej Zywno <m.zywno@gmail.com>
2016-03-04 14:56:11 +0100

    uber

---
+++ app/models/uber.rb
@@ -0,0 +1,10 @@
+class Uber
+  def client
+    client = Uber::Client.new do |config|
+      config.server_token  = 'wk8nwLSCOQUMAKUbtSQYfISSk8yosMzDeB8RnHhN'
+      config.client_id     = 'pDgNR8fvL2s3qarXh23IamF7zWTrtRCd'
+      config.client_secret = 'wAC-f6nfNOrqoz-APF84lNL37R7eFCEM8tC6ORT4'
+    end
+  end
+
+end
