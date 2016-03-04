5cb72eabfc18d7e80fb9adf33c2dfc90852e7a70
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-03-04 10:53:12 +0100

    Cleanup

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -5,16 +5,16 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', current_user.name
     end
 
     protected
-      def find_verified_user
-        if verified_user = User.find_by(id: cookies.signed['user.id'])
-          verified_user
-        else
-          reject_unauthorized_connection
-        end
+
+    def find_verified_user
+      if verified_user = User.find_by(id: cookies.signed['user.id'])
+        verified_user
+      else
+        reject_unauthorized_connection
       end
+    end
   end
 end
