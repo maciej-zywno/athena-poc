1277d2c81b83a36bc8710d0a83eca16e5c2ba9e0
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-03-14 09:27:00 +0100

    Refactor

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,29 +1,31 @@
 class AppointmentsController < ApplicationController
   include Wicked::Wizard
 
-  steps :set_department_and_provider, :set_appointment_reason, :set_appointment_slot
+  steps :set_appointment_reason, :set_appointment_slot
+
+  def new
+    @providers = athena_health_client.all_providers(
+      practice_id: params[:practice_id]
+    ).providers
+
+    @departments = athena_health_client.all_departments(
+      practice_id: params[:practice_id]
+    ).departments
+  end
 
   def show
     case step
-    when :set_department_and_provider
-      @providers = athena_health_client.all_providers(
-        practice_id: params[:practice_id]
-      ).providers
-
-      @departments = athena_health_client.all_departments(
-        practice_id: params[:practice_id]
-      ).departments
     when :set_appointment_reason
       @patient_appointment_reasons = athena_health_client.all_patient_appointment_reasons(
         practice_id: params[:practice_id],
-        department_id: params[:appointment][:department_id],
-        provider_id: params[:appointment][:provider_id]
+        department_id: params[:department_id],
+        provider_id: params[:provider_id]
       ).patient_appointment_reasons
 
     when :set_appointment_slot
       @appointment_slots = athena_health_client.open_appointment_slots(
         practice_id: params[:practice_id],
-        reason_id: params[:appointment][:patient_appointment_reason_id],
+        reason_id: params[:patient_appointment_reason_id],
         provider_id: params[:provider_id],
         department_id: params[:department_id]
       ).appointments
@@ -37,7 +39,7 @@ class AppointmentsController < ApplicationController
       practice_id: params[:practice_id],
       reason_id: params[:patient_appointment_reason_id],
       patient_id: 1,
-      appointment_id: params[:appointment][:appointment_id]
+      appointment_id: params[:appointment_id]
     )
 
     redirect_to root_path, notice: 'visit booked'
